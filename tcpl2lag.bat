f17ca1f5bde12466cc64df4406e7a597233cc606
MaksimKayumov <81908868+MaksimKayumov@users.noreply.github.com>
2022-07-06 20:40:17 +0500

    replace % with !

--- tcpl2lag.bat
+++ tcpl2lag.bat
@@ -4,16 +4,15 @@ setlocal enabledelayedexpansion
 :: dec;x = decrement x
 :: start;x;y = start loop x while y is not zero
 :: end;x;y = end loop x
-@echo off
 set cmd=0
 set v=%%
 FOR /F "delims=; tokens=1,2,3,4,5,6,7" %%a in (%1) do (
 	set /a cmd=!cmd!+1
 	set /a cmdnext=!cmd!+1
 	if %%a == inc (
-		echo !cmd!;%%b;-1;%cmd%;>>%1.lag
+		echo !cmd!;%%b;-1;!cmd!;>>%1.lag
 	) else if %%a == dec (
-		echo !cmd!;%%b;1;%cmdnext%;>>%1.lag
+		echo !cmd!;%%b;1;!cmdnext!;>>%1.lag
 	) else if %%a == start (
 		echo start%%b;%%c;0;end%%b;>>%1.lag
 	) else if %%a == end (
