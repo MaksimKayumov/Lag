a812275d36ea7450ee7fe6fe4fac4d009d880ca6
MaksimKayumov <81908868+MaksimKayumov@users.noreply.github.com>
2022-07-06 19:07:12 +0500

    Update lag.bat

--- lag.bat
+++ lag.bat
@@ -1,11 +1,10 @@
 @echo off
 set v=%%
 echo @echo off>%1.bat
-	FOR /F "delims=; tokens=1,2,3,4" %%a in (%1) do (
-		echo :%%a>>%1.bat
-		echo set /a %%b=%v%%%b%v%-%%c>>%1.bat
-		echo if %v%%%b%v% == 0 goto %%d>>%1.bat
+FOR /F "delims=; tokens=1,2,3,4" %%a in (%1) do (
+	echo :%%a>>%1.bat
+	echo set /a %%b=%v%%%b%v%-%%c>>%1.bat
+	echo if %v%%%b%v% == 0 goto %%d>>%1.bat
 
-	)
 )
 echo Program Compiled.
